4b8af75da12fe284ba1228452a02782f591cbaeb
shideshan <307042873@qq.com>
2021-11-05 21:06:03 +0800

    feature-eslint、prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import '@css/index.scss'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import '@css/index.scss';
 
 try {
-    ReactDOM.render(<App/>, document.getElementById('root'))
+  ReactDOM.render(<App />, document.getElementById('root'));
 } catch (error) {
-    console.log(error)
+  console.log(error);
 }
