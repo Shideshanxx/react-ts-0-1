aa1c4feec110307b36282225d3c296391d1677fe
shideshan <307042873@qq.com>
2021-11-05 22:14:25 +0800

    fix: 修复commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,46 +1,24 @@
-/**
- * git commit -m <type>[optional scope]: <description>
-type ：用于表明我们这次提交的改动类型，是新增了功能？还是修改了测试代码？又或者是更新了文档？总结以下 11 种类型：
-
-build：主要目的是修改项目构建系统(例如 glup，webpack，rollup 的配置等)的提交
-ci：主要目的是修改项目继续集成流程(例如 Travis，Jenkins，GitLab CI，Circle等)的提交
-docs：文档更新
-feat：新增功能
-fix：bug 修复
-perf：性能优化
-refactor：重构代码(既没有新增功能，也没有修复 bug)
-style：不影响程序逻辑的代码修改(修改空白字符，补全缺失的分号等)
-test：新增测试用例或是更新现有测试
-revert：回滚某个更早之前的提交
-chore：不属于以上类型的其他类型(日常事务)
-
-optional scope：一个可选的修改范围。用于标识此次提交主要涉及到代码中哪个模块。
-
-description：一句话描述此次提交的主要内容，做到言简意赅。
-*/
-
-// rule由name和配置数组组成，如：'name:[0, 'always', 72]'，
-// 数组中第一位为level，可选0,1,2，0为disable，1为warning，2为error；
-// 第二位为应用与否，可选always|never；
-// 第三位该rule的值。
-
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
+    // 'subject-case': [0, 'never'],
     'type-enum': [
-      2,
+      2, //代表必须输入
       'always',
       [
-        'bug', // 此项特别针对bug号，用于向测试反馈bug列表的bug修改情况
-        'feature', // 新功能（feature）
-        'fix', // 修补bug
-        'docs', // 文档（documentation）
-        'style', // 格式（不影响代码运行的变动）
-        'refactor', // 重构（即不是新增功能，也不是修改bug的代码变动）
-        'test', // 增加测试
-        'chore', // 构建过程或辅助工具的变动
-        'revert', // feat(pencil): add ‘graphiteWidth’ option (撤销之前的commit)
-        'merge', // 合并分支， 例如： merge（前端页面）： feature-xxxx修改线程地址
+        'docs', // Adds or alters documentation. 仅仅修改了文档，比如README, CHANGELOG, CONTRIBUTE等等
+        'chore', // Other changes that don't modify src or test files. 改变构建流程、或者增加依赖库、工具等
+        'feat', // Adds a new feature. 新增feature
+        'fix', // Solves a bug. 修复bug
+        'merge', // 仅进行分支合并.
+        'improvement', // 用于对当前实现进行改进而没有添加新功能或修复错误的提交.
+        'perf', // Improves performance. 优化相关，比如提升性能、体验
+        'refactor', // Rewrites code without feature, performance or bug changes. 代码重构，没有加新功能或者修复bug
+        'revert', // Reverts a previous commit. 回滚到上一个版本
+        'style', // Improves formatting, white-space. 仅仅修改了空格、格式缩进、逗号等等，不改变代码逻辑
+        'test', // Adds or modifies tests. 测试用例，包括单元测试、集成测试等
+        'ci', //主要目的是修改项目继续集成流程(例如 Travis，Jenkins，GitLab CI，Circle等)的提交
+        'build', //主要目的是修改项目构建系统(例如 glup，webpack，rollup 的配置等)的提交
       ],
     ],
   },
