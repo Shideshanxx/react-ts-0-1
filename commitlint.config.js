84e90b498b3c9c4c278526d93a7e3f9b8b33b573
shideshan <307042873@qq.com>
2021-11-05 21:29:25 +0800

    commitlint修复

--- commitlint.config.js
+++ commitlint.config.js
@@ -26,18 +26,4 @@ description：一句话描述此次提交的主要内容，做到言简意赅。
 
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  rules: {
-    'type-enum': [
-      2,
-      'always',
-      ['upd', 'feat', 'fix', 'refactor', 'docs', 'chore', 'style', 'revert'],
-    ],
-    'type-case': [0],
-    'type-empty': [0],
-    'scope-empty': [0],
-    'scope-case': [0],
-    'subject-full-stop': [0, 'never'],
-    'subject-case': [0, 'never'],
-    'header-max-length': [0, 'always', 72],
-  },
 };
